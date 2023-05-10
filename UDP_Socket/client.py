9c46b2ff57a007dbf7bcca0b633e7cd19388eab6
ak4m410x01 <abdallah10kamal@gmail.com>
2023-05-10 22:41:22 +0300

    UDP_Socket Connection Refactor Code v1.1

--- UDP_Socket/client.py
+++ UDP_Socket/client.py
@@ -1,25 +1,18 @@
 import socket
 
 MAX_BUFFER_SIZE = 1024
+SERVER_ADDR = ("127.0.0.1", 55555)
 
 
-def client_socket_udp(_server_ip: str, _server_port: int) -> None:
-    # Create Socket
-    _client_connection_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Create Socket Connection
+client_connection_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Send Data to Server
-    _client_msg = "Hi, Server".encode("ASCII")
-    _server_addr = (_server_ip, _server_port)
-    _client_connection_socket.sendto(_client_msg, _server_addr)
+# Send Data to Server
+client_msg = "Hi, Server".encode("ASCII")
+client_connection_socket.sendto(client_msg, SERVER_ADDR)
 
-    # Recv Data from Server
+# Recv Data from Server
 
-    _server_msg, addr = _client_connection_socket.recvfrom(MAX_BUFFER_SIZE)
+_server_msg, addr = client_connection_socket.recvfrom(MAX_BUFFER_SIZE)
 
-    print(f"{_server_addr} => {_server_msg.decode('ASCII')}")
-
-
-server_ip = socket.gethostbyname(socket.gethostname())
-server_port = 55555
-
-client_socket_udp(server_ip, server_port)
+print(f"{SERVER_ADDR} => {_server_msg.decode('ASCII')}")
--- UDP_Socket/server_broadcast.py
+++ UDP_Socket/server_broadcast.py
@@ -1,45 +1,36 @@
 import socket
 
 MAX_BUFFER_SIZE = 1024
+SERVER_ADDR = ("0.0.0.0", 55555)
 
 
-def server_socket_udp(_server_ip: str, _server_port: int) -> None:
-    # Create Socket
-    _server_connection_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Create Socket Connection
+server_connection_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Broadcast Option
-    _server_connection_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+# Set Sock Option SO_BROADCAST = 1
+server_connection_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-    # Bind Socket with (serverIP,serverPort)
-    _server_addr = (_server_ip, _server_port)
-    _server_connection_socket.bind(_server_addr)
+# Bind Socket with SERVER_ADDR
+server_connection_socket.bind(SERVER_ADDR)
 
-    while True:
-        try:
-            print("=====================================")
-            print("\n Waiting...!!")
+while True:
+    try:
+        print("=====================================")
+        print("Waiting Client(s)...!!\n")
 
-            # Recv Data from client(s)...
-            _client_msg, _client_addr = _server_connection_socket.recvfrom(
-                MAX_BUFFER_SIZE
-            )
+        # Recv Data from client(s)...
+        client_msg, client_addr = server_connection_socket.recvfrom(MAX_BUFFER_SIZE)
 
-            print(f"{_client_addr} => {_client_msg.decode('ASCII')}")
+        print(f"{client_addr} => {client_msg.decode('ASCII')}")
 
-            # Send Data to Client(s)
-            _server_msg = "Hi, Client\n".encode("ASCII")
-            _server_connection_socket.sendto(_server_msg, _client_addr)
+        # Send Data to Client(s)
+        server_msg = "Hi, Client\n".encode("ASCII")
+        server_connection_socket.sendto(server_msg, client_addr)
 
-            print(f"{_server_addr} => {_server_msg.decode('ASCII')}")
+        print(f"{SERVER_ADDR} => {server_msg.decode('ASCII')}")
 
-        except KeyboardInterrupt:
-            print("***************************")
-            print("   Connection Closed :(    ")
-            print("***************************")
-            break
-
-
-server_ip = "<broadcast>"
-server_port = 55555
-
-server_socket_udp(server_ip, server_port)
+    except KeyboardInterrupt:
+        print("***************************")
+        print("   Connection Closed :(    ")
+        print("***************************")
+        break
