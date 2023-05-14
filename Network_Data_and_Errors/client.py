acab546abf224c51204e6d23a4dc77c0ae9ce32b
ak4m410x01 <abdallah10kamal@gmail.com>
2023-05-14 11:38:32 +0300

    Clinet TCP

--- Network_Data_and_Errors/client.py
+++ Network_Data_and_Errors/client.py
@@ -1,11 +1,11 @@
-import socket
-import struct
+from socket import *
+from struct import *
 
 SERVER_ADDR = ("127.0.0.1", 55555)
 
 
-def send_all(msg: str):
-    header_struct = struct.Struct("!I")
+def send_all(msg: str) -> None:
+    header_struct = Struct("!I")
 
     msg_size = len(msg)
 
@@ -16,14 +16,14 @@ def send_all(msg: str):
     client_socket_connection.sendall(msg.encode())
 
 
-# =========================================================================
-
 # Create Socket Connection
-client_socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket_connection = socket(AF_INET, SOCK_STREAM)
 
 # Connect with server
 client_socket_connection.connect(SERVER_ADDR)
 
+# Close Recv option
+client_socket_connection.shutdown(SHUT_RD)
 
 send_all("Hello, Server")
 send_all("Hi, server")
