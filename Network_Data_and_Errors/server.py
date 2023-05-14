f09781948270aa461873196c38f70cc7fa5d161c
ak4m410x01 <abdallah10kamal@gmail.com>
2023-05-14 11:38:21 +0300

    server TCP

--- Network_Data_and_Errors/server.py
+++ Network_Data_and_Errors/server.py
@@ -1,60 +1,77 @@
-import socket
-import struct
+from socket import *
+from struct import *
 
 SERVER_MAX_CONNECTION = 5
 SERVER_ADDR = ("127.0.0.1", 55555)
 
 
 def recv_all():
-    header_struct = struct.Struct("!I")
+    header_struct = Struct("!I")
 
     # Recv Msg Size
     msg_size = client_socket_connection.recv(header_struct.size)
 
-    # Unpack msg size (convert from bytes into integer)
+    # unpack msg size (convert from bytes to integer)
     msg_size = header_struct.unpack(msg_size)
 
-    msg = client_socket_connection.recv(msg_size[0])
+    # Send Msg Size
+    client_msg = client_socket_connection.recv(msg_size[0])
 
-    return msg
+    return client_msg
 
-
-# =========================================================================
 
 # Create Socket Connection
-server_socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket_connection = socket(AF_INET, SOCK_STREAM)
 
 # Set sock option REUSEADDR = 1
-server_socket_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket_connection.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
 # Bind Socket with server addr
 server_socket_connection.bind(SERVER_ADDR)
 
 # Listing...
 server_socket_connection.listen(SERVER_MAX_CONNECTION)
-print("===========================")
-print(f"Listing at:{SERVER_ADDR}")
-print("---------------------------")
 
-# Accept Connection
-client_socket_connection, client_addr = server_socket_connection.accept()
-
-# Close Sending...
-client_socket_connection.shutdown(socket.SHUT_WR)
-
-
-client_msg = ""
+print(f"Listing at: `{SERVER_ADDR}`")
 
 while True:
-    msg = recv_all()
+    try:
+        print("=====================================")
+        print("Waiting Client(s)...!!\n")
 
-    if not msg:
+        # Accept Connection
+        client_socket_connection, client_addr = server_socket_connection.accept()
+
+        # Close Sending...
+        client_socket_connection.shutdown(SHUT_WR)
+
+        client_msg = ""
+
+        while True:
+            try:
+                msg = recv_all()
+
+                if not msg:
+                    break
+
+                client_msg += f"{msg.decode()}\n"
+
+            except error:
+                print(f"error while recving\nError:{error}")
+
+        print(f"Received From {client_addr}:\n{client_msg}")
+
+        # Close Connection
+        client_socket_connection.close()
+
+    except KeyboardInterrupt:
+        print("***************************")
+        print("   Connection Closed :(    ")
+        print("***************************")
+
+        # Close Server Socket Connection
+        server_socket_connection.close()
         break
 
-    client_msg += f"{msg.decode()}\n"
-
-
-print(f"{client_addr}:\n{client_msg}")
-
-# Close Connection
-client_socket_connection.close()
+    except error:
+        print(f"Client Connection Error:{error}")
